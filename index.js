b5d3525bf90b35af6881ef7faf576899ca92bcf9
K-ash-ish <kashishsondhiya@gmail.com>
2021-12-30 20:14:29 +0530

    javascript file to add books

--- index.js
+++ index.js
@@ -1,2 +1,51 @@
 
+const bookName = document.querySelector("#book-name");
+const authorName = document.querySelector("#author-name");
+const totalPages = document.querySelector("#total-pages");
+const readStatus = document.querySelector("#read-status");
+const tableData = document.querySelector(".table-data");
+const addBook = document.querySelector(".add-book");
+
+let myLibrary = [
+
+];
+
+
+window.addEventListener("DOMContentLoaded", function(){
+    displayBook();
+
+});
+
+addBook.addEventListener("click", function(){
+    Book();
+    displayBook();
+});
+
+function displayBook(){
+    let displayBooks = myLibrary.map(function(item){
+        console.log(item);
+        return `
+        <tr>
+            <td class="table-data name">${item.name}</td>
+            <td class="table-data author">${item.author}</td>
+            <td class="table-data pages">${item.pages}</td>
+            <td class="table-data read">${item.read}</td>
+        </tr>   
+        `;
+    });
+    displayBooks = displayBooks.join("");
+    tableData.innerHTML = displayBooks;
+    return displayBooks;
+}
+
+function Book(){
+    let book = {};
+    book.name= bookName.value;
+    book.author = authorName.value;
+    book.pages = totalPages.value;
+    book.read = readStatus.value;
+    myLibrary.push(book);
+}
+
+
 
