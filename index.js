3cc4675d165347b226ad8f5b12cb7c7d3591d767
K-ash-ish <kashishsondhiya@gmail.com>
2022-01-19 13:33:47 +0530

    taking input from user

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ class BookList {
         new Books("SpiderMan", "Stan Lee", 123, "Read" ),
         new Books("IronMan", "Stan Lee", 223, "Not Read" )
     ];
-
+    
     render(){
         const renderHook = document.querySelector(".table-data");
         for (const book of this.myLibrary){
@@ -29,11 +29,25 @@ class BookList {
         }
     }
 }
-class UserInput{
+class AddBooks  extends BookList{
+    userInput(){
+        // const addbtn = document.querySelector(".add-book");
+        document.querySelector("#book-form").addEventListener("submit", function(e){
+            e.preventDefault();
+            const bookName = document.getElementById("book-name").value;
+            const authorName = document.getElementById("author-name").value;
+            const pages = document.getElementById("total-pages").value;
+            const readStatus = document.getElementById("read-status").value;
+            const newBook = new Books(bookName, authorName, pages, readStatus)
+            console.log(newBook)
+        } );
+    }
 
 }
 
 const bookList = new BookList();
 bookList.render();
+const addBooks = new AddBooks();
+addBooks.userInput();
 
 
